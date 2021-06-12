5e1eeac512b50a165227be2b320ba20ac12997d5
han <consistant1y@ajou.ac.kr>
2021-06-12 17:22:54 +0900

    Update jest.config.js

--- server_node/jest.config.js
+++ server_node/jest.config.js
@@ -1,9 +1,23 @@
 module.exports = {
     testEnvironment: 'node',
     setupFiles: ['./jest.setup.js'], // <- add this
-
+    
+    coverageDirectory: 'output/coverage/',
+    coverageReporters: ['text-summary', 'html'],
+    
     moduleDirectories: [
         "node_modules",
         "src"
-    ]
+    ],
+    
+    reporters: [
+        'default',
+        [
+          './node_modules/jest-html-reporter',
+          {
+            pageTitle: 'Test Report',
+            outputPath: 'output/test/test-report.html',
+          },
+        ],
+    ],
 };
